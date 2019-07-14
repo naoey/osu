95a88f6242fc0eb193b038884ba5f53c4e11672b
naoey <naoey@protonmail.ch>
2019-07-14 22:57:30 +0530

    Create a historical graph component

---
+++ osu.Game.Tests/Visual/UserInterface/TestSceneHistoricalGraph.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using osu.Framework.MathUtils;
+using osu.Game.Graphics.UserInterface;
+using osu.Framework.Graphics;
+
+namespace osu.Game.Tests.Visual.UserInterface
+{
+    public class TestSceneHistoricalGraph : OsuTestScene
+    {
+        public override IReadOnlyList<Type> RequiredTypes => new[]
+        {
+            typeof(HistoricalGraph),
+        };
+
+        private readonly HistoricalGraph graph;
+
+        public TestSceneHistoricalGraph()
+        {
+            Add(graph = new HistoricalGraph
+            {
+                Width = 300,
+                Height = 180,
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+            });
+
+            AddStep("Create values", () =>
+            {
+                var values = new List<KeyValuePair<DateTimeOffset, float>>();
+
+                for (int i = 0; i < 5; i++)
+                {
+                    values.Add(new KeyValuePair<DateTimeOffset, float>(DateTimeOffset.Now.AddDays(-i), RNG.NextSingle() * 100));
+                }
+
+                graph.Values = values;
+            });
+        }
+    }
+}
---
+++ osu.Game/Graphics/UserInterface/HistoricalGraph.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics;
+using System.Linq;
+using osuTK;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Allocation;
+
+namespace osu.Game.Graphics.UserInterface
+{
+    public class HistoricalGraph : Container
+    {
+        private IEnumerable<KeyValuePair<DateTimeOffset, float>> values;
+
+        public IEnumerable<KeyValuePair<DateTimeOffset, float>> Values
+        {
+            get => values ?? Array.Empty<KeyValuePair<DateTimeOffset, float>>();
+            set
+            {
+                if (value.Equals(values))
+                    return;
+
+                values = value;
+
+                line.Values = values.Select(v => v.Value);
+
+                updateRulers();
+            }
+        }
+
+        private LineGraph line;
+        private FillFlowContainer verticalRulersContainer;
+        private FillFlowContainer horitontalRulersContainer;
+
+        public HistoricalGraph()
+        {
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(OsuColour colours)
+        {
+            Children = new Drawable[]
+            {
+                verticalRulersContainer = new FillFlowContainer
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Direction = FillDirection.Horizontal,
+                },
+                horitontalRulersContainer = new FillFlowContainer
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Direction = FillDirection.Vertical,
+                },
+                line = new LineGraph
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Colour = colours.YellowDark,
+                },
+            };
+        }
+
+        private void updateRulers()
+        {
+            verticalRulersContainer.Clear(true);
+            horitontalRulersContainer.Clear(true);
+
+            var dates = Values.Select(v => v.Key);
+            var counts = Values.Select(v => v.Value);
+
+            verticalRulersContainer.Spacing = new Vector2(DrawWidth / dates.Count());
+            horitontalRulersContainer.Spacing = new Vector2(DrawHeight / counts.Count());
+
+            foreach (var date in dates)
+                verticalRulersContainer.Add(new Box
+                {
+                    RelativeSizeAxes = Axes.Y,
+                    Width = 1,
+                    Colour = OsuColour.FromHex("#FFF"),
+                    Alpha = 0.4f,
+                });
+
+            foreach (var count in counts)
+                horitontalRulersContainer.Add(new Box
+                {
+                    RelativeSizeAxes = Axes.X,
+                    Height = 1,
+                    Colour = OsuColour.FromHex("#FFF"),
+                    Alpha = 0.4f,
+                });
+        }
+    }
+}
